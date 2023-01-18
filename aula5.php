a316f1b11198ec6b41221e057ed87053f827aa18
Tiago <ggtiago0santos@gmail.com>
2023-01-18 12:40:34 +0000

    UIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

--- aula5.php
+++ aula5.php
@@ -1,16 +1,53 @@
-<?php   
+<?php
 //$_POST ou $_GET
+/*
 if(isset($_POST['email']) && isset($_POST['info']))
     if (!empty($_POST['email'])) 
         echo "Email: ".$_POST['email']. " Info: ".$_POST['info'];
-
+*/
+/*
 echo $_SERVER['HTTP_USER_AGENT'];
 echo $_SERVER['SERVER_PORT'];
 echo $_SERVER['REMOTE_METHOD'];
 echo $_SERVER['PHP_SELF'];
-
+*/
+/*
 $nextWeek = time() + (7 * 24 * 60 * 60);
-echo 'Now:       '. date('Y-m-d')."\n";
+echo 'Now:'. date('Y-m-d')."\n";
+echo 'Next Week:'. date('Y-m-d', $nextWeek)."\n";
+$hora = date('H:i:s');
+echo $hora;
+*/
+/*
+if (isset($_POST['submit'])) {
+    red();
+}
+
+function red() {
+    $host = $_SERVER['HTTP_HOST'];
+    echo $host.'<br>';
+    $uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    echo $uri.'<br>';
+    $extra = 'aula4.php';
+    header("Location: http://$host$uri/$extra");
+}
+
+*/
+//setcookie('nome', 'valor', time()+3600);
+
+setcookie ("cookie3", "cookiethree");
+setcookie ("cookie2", "cookietwo");
+setcookie ("cookie1", "cookieone");
+
+/*
+if (isset($_COOKIE)){
+    print_r($_COOKIE);
+}*/
+
+/* echo $_COOKIE["cookie2"];*/
+
+$umDia = time() + 86400;
+setcookie ("NOMEUSER", "Joao", $umDia);
 
 ?>
 <html>
--- fiveserver.config.js
+++ fiveserver.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  php: "C:\\xampp\\php\\php.exe",
+  php: "C:\\Users\\Tiago\\Documents\\php\\php\\php.exe",
 };
