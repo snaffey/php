2576803e283558836795530014abae1e3e405b76
Tiago <68923057+snaffey@users.noreply.github.com>
2023-01-28 18:34:31 +0000

    yup

--- aula7sql.php
+++ aula7sql.php
@@ -8,11 +8,11 @@
             Cria uma conexão com o servidor referenciando no 1 argumento, segundo argumento user que tem que estar registado no servidor referido, bem como a pass(3 argumento) referente ao user, o ultimo referencia a BD
             mysqli_connect(domain, user, pass, db)
             */
-            $conexao = mysqli_connect('localhost','root','','desafioprog21');
+            $conexao = mysqli_connect('localhost','root','','teste');
 
-            mysqli_select_db($conexao, 'desafioprog21');
+            mysqli_select_db($conexao, 'teste');
 
-            $consulta = 'SELECT id, nome, dataNasc, salario FROM aluno';
+            $consulta = 'SELECT id, nome, dataNasc FROM Aluno';
 
             // mysqli_query() executa a query
 
@@ -22,12 +22,12 @@
                 echo "Numero de linhas: $numLinhas <br>";
                 //mysqli_fetch_row() retorna um array com os dados da linha
                 while ($linha = mysqli_fetch_row($resultado)) {
-                    echo "ID: $linha[0] - Nome: $linha[1] - Data de Nascimento: $linha[2] - Salario: $linha[3] <br>";
+                    echo "ID: $linha[0] - Nome: $linha[1] - Data de Nascimento: $linha[2] <br>";
                 }
             }
 
-            echo "Conexão:".mysqli_get_host_info ($conexao)."<br>";
-            echo "Versão do servidor:".mysqli_get_server_info ($conexao)."<br>";
+            echo "Conexão: ".mysqli_get_host_info ($conexao)."<br>";
+            echo "Versão do servidor: ".mysqli_get_server_info ($conexao)."<br>";
          ?>
     </body>
 </html>
--- fiveserver.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  php: "/home/codespace/.php/current/bin/php",
-};
