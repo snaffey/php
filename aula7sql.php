dec3144fb1171aa46ecbaba4bdde01bd72b486c8
Tiago <68923057+snaffey@users.noreply.github.com>
2023-01-30 11:12:35 +0000

    fim

--- aula7sql.php
+++ aula7sql.php
@@ -27,7 +27,46 @@
             }
 
             echo "Conexão: ".mysqli_get_host_info ($conexao)."<br>";
-            echo "Versão do servidor: ".mysqli_get_server_info ($conexao)."<br>";
+            echo "Versão do servidor: ".mysqli_get_server_info ($conexao)."<br>"."<br>";
+
+            // 2 consulta mais complexa
+            $consulta = "SELECT id, nome FROM Aluno WHERE nome LIKE 'Ti%'";
+            $resultado = mysqli_query($conexao, $consulta);
+            echo "<div>Alunos com nome a começar por Ti<br>";
+            while ($linha = mysqli_fetch_row($resultado)) {
+                echo "ID: $linha[0] - Nome: $linha[1] <br>";
+            }
+            echo "</div>"."<br>";
+
+            // Criar uma função para fazer a consulta
+            // O argumento query vai conter a identificação e o resultado da query
+            // o argumento $conexao é a conexão com a BD
+
+            function executaQuery($conexao, &$query){
+                echo "Executa atraves de uma função<br>Conexão: ".mysqli_get_host_info ($conexao)."<br>";
+                echo "<div>Nome";
+                while ($linha = mysqli_fetch_row($query)) {
+                    echo "ID: $linha[0] - Nome: $linha[1] <br>";
+                }
+                echo "</div>";
+            }
+            $consulta = "SELECT id, nome FROM Aluno WHERE nome LIKE '%ago%'";
+            $resultado = mysqli_query($conexao, $consulta);
+            executaQuery($conexao, $resultado);
+            echo "<br>";
+
+            // Usando a destruição explicita die()
+            // Caso devolva erro a conexão com o servidor é chamado
+            //mysqli_connect('localhost','root','','teste') or die(mysql_error());
+
+            $conn = mysqli_connect('localhost','tiago','123','teste') or die(mysql_error());
+            if (!$conn) {
+                die(mysql_error());
+            }
+            $consulta = "SELECT id, nome FROM Aluno WHERE nome LIKE '%ago%'";
+            $resultado = mysqli_query($conn, $consulta);
+            executaQuery($conn, $resultado);
+
          ?>
     </body>
 </html>
