8321baa645ca7ed4787ce66a8dc4bf59bd60119e
Tiago <68923057+snaffey@users.noreply.github.com>
2023-01-30 11:22:21 +0000

    hm

---
+++ admin_projeto/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>My Page</title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="js/home.js"></script>
+    </head>
+
+    <body>
+        <header class="header">
+            <img src="./img/logo.png" alt="logo" class="logo"/>
+            Menu
+        </header>
+        <section class="location_banner">
+            <h1>Melhores casas e apartamentos em um só lugar</h1>
+        </section>
+        <div class="admin">
+            <a href="admin.php">Admin</a>
+        </div>
+        <main>
+            <article>
+                <img src="./img/casa.jpg" alt="casa" class="casa"/>
+                <h2>Casa com jardinagem</h2>
+                <a href="ver.php">Ver mais</a>
+            </article>
+            <article>
+                <img src="./img/casa.jpg" alt="casa" class="casa"/>
+                <h2>Casa com jardinagem</h2>
+                <a href="ver.php">Ver mais</a>
+            </article>
+            <article>
+                <img src="./img/casa.jpg" alt="casa" class="casa"/>
+                <h2>Casa com jardinagem</h2>
+                <a href="ver.php">Ver mais</a>
+            </article>
+        </main>
+        <footer>
+            <div>
+                Copyrigth Prog&deg;2
+            </div>
+        </footer>
+    </body>
+
+</html>
