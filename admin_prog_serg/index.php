38400e402e559175473f9d051bec72bbc7f8aec5
Tiago <68923057+snaffey@users.noreply.github.com>
2023-02-24 18:49:38 +0000

    sfd

--- admin_prog_serg/index.php
+++ admin_prog_serg/index.php
@@ -1,40 +1,45 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset = "UTF-8">
-		<title>Home PaGE</title>
-		<link rel="stylesheet" href="./css/estilo.css" >
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-		<script src="./js/home.js">
-		</script>
-	</head>
-	<body>
-		 <header class="header">
-			<img src="./img/logo.jpg" alt="Logo empresa" />
-		 </header>
-		 <section class="section_banner">
-			<h1>Aqui encontra as melhores casas</h1>
-		 </section>
-		 <div class="admin">
-			<a href="admin.php">
-			<img src="./img/admin.png" alt="Login" />
-			</a>
-		 </div>
-	<?php include_once './functionDB.php'; ?>
-	<?	$lista = get_imoveis_list(); ?>		
-		 <main class="imoveis">
-		 <?php foreach ($lista as $data): ?>
-			<article>
-				<img src="<?=$data['imgPath'] ?>"alt="<?=$data['altimg'] ?>" />
-				<h2><?=$data['descricao'] ?></h2>
-				<a href="ver.php?id=<?=$data['id'] ?>">mais informação...</a>
-			</article>
-		<?php endforeach; ?> 
-		 </main>
-		 <footer>
-			<div>
-				Copyright Prog&deg;2
-			</div>
-		 </footer>
-	</body>
+  <head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <script src="<https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js>"></script>
+    <script src="./js/home.js"></script>
+  </head>
+  <body>
+    <header class="header">
+      <img src="./img/logo.png" alt="Logo">
+      <nav class="menu">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </nav>
+    </header>
+    <section class="section-banner">
+      <h1>Find Your Dream Home</h1>
+    </section>
+    <div class="admin">
+      <a href="admin.php">
+        <img src="./img/admin.png" alt="Login">
+      </a>
+    </div>
+    <?php include_once './functionDB.php'; ?>
+    <?php $lista = get_imoveis_list(); ?>
+    <main class="imoveis">
+      <?php foreach ($lista as $data): ?>
+      <article>
+        <img src="<?=$data['imgPath']?>" alt="<?=$data['altimg']?>">
+        <h2><?=$data['descricao']?></h2>
+        <a href="ver.php?id=<?=$data['id']?>" class="btn">More Info</a>
+      </article>
+      <?php endforeach; ?>
+    </main>
+    <footer>
+      <div class="footer-bottom">
+        <p>&copy; 2021 Tiago</p>
+      </div>
+    </footer>
+  </body>
 </html>
