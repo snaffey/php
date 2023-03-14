d95f13011e201a9f957e07b8cfe1114a50c8e9c0
Tiago <68923057+snaffey@users.noreply.github.com>
2023-03-14 11:22:01 +0000

    Mar 14, 2023, 11:22 AM

--- PHP_Projeto/Iterator/UtentesCalheta.php
+++ PHP_Projeto/Iterator/UtentesCalheta.php
@@ -5,11 +5,7 @@ include_once './IteratorListaUtentesCalheta.php';
 class UtentesCalheta implements AgregadoUtentes {
 	protected $utentes;
 	public function __construct() {
-		$this->utentes = array();
-        $this->utentes[] = new Utente("Utente 1 Calheta");
-		$this->utentes[] = new Utente("Utente 2 Calheta");
-		$this->utentes[] = new Utente("Utente 3 Calheta");
-		$this->utentes[] = new Utente("Utente 4 Calheta");
+		
 	}
 	public function criarIterator() {
 		return new IteratorListaUtentesCalheta($this->utentes);
