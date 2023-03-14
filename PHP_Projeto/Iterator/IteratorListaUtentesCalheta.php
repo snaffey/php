12e851fa5b5d9ad75f8dfad18e52333d5a4e0854
Tiago <68923057+snaffey@users.noreply.github.com>
2023-03-14 11:11:41 +0000

    Mar 14, 2023, 11:11 AM

---
+++ PHP_Projeto/Iterator/IteratorListaUtentesCalheta.php
@@ -0,0 +1,28 @@
+<?
+include_once './IteratorInterface.php';
+
+class IteratorListaUtentesCalheta implements IteratorInterface {
+	protected $lista = array();
+	protected $contador;
+	 public function __construct($lista) {
+        $this->lista = $lista;
+        $this->contador = 0;
+    }
+	public function first() {
+		$this->contador = 0;
+	}
+	public function next() {
+		 $this->contador++;
+	}
+	public function isDone() {
+		return $this->contador == count($this->lista);
+	}
+	public function currentItem() {
+		if ($this->isDone()) {
+			$this->contador = count($this->lista) - 1;
+		}else if ($this->contador < 0){
+			$this->contador = 0;
+		}
+		return $this->lista[$this->contador];
+	}
+}
---
+++ PHP_Projeto/Iterator/Utente.php
@@ -0,0 +1,9 @@
+<?
+class Utente {
+	public $nome;
+	public function __construct($nome) {
+        $this->nome = $nome;
+    }
+}
+?>
+	
