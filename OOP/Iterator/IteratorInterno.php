84f58d12cf60435e4aca3d61c6bed145fd86b857
Tiago <68923057+snaffey@users.noreply.github.com>
2023-03-14 09:24:32 +0000

    Mar 14, 2023, 9:24 AM

--- OOP/Iterator/IteratorInterno.php
+++ OOP/Iterator/IteratorInterno.php
@@ -1,7 +1,14 @@
 <?php
-include_once './IteratorInteface.php';
+include_once './IteratorInterface.php';
 
 abstract class IteratorInterno {
+    protected $it;
+    public function loopList(){
+        for ($this->it->first(); $this->it->isDone(); $this->it->next()){
+            $this->operation($this->it->currentItem());
+        }
+    }
+
     abstract protected function operation($utente);
 }
 
